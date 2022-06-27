0390d97cbaf7469f751bbe55dab927c2ba786ee7
muzyk0 <ru9art@gmail.com>
2022-06-27 22:11:39 +0300

    add .env template generator

---
+++ src/types/env.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      BASE_URL: string;
+      MONGO_URI: string;
+      ACCESS_TOKEN_SECRET: string;
+      EMAIL_FROM: string;
+      EMAIL_FROM_PASSWORD: string;
+    }
+  }
+}
+
+export {}

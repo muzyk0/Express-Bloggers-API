1b81392c0fce9cdbe5a2aaf0b70697d34961fbae
muzyk0 <ru9art@gmail.com>
2022-07-06 23:15:43 +0300

    fix email confirmation template link

--- src/domain/email-template-manager.ts
+++ src/domain/email-template-manager.ts
@@ -6,7 +6,7 @@ export const emailTemplateManager = {
         return `<div>
             <h1>Thanks for registration ${user.accountData.login}</h1>
             <h3>Confirm your email</h3>
-            <a href="${BASE_URL}/code=${user.emailConfirmation.confirmationCode}">${BASE_URL}/code=${user.emailConfirmation.confirmationCode}</a>
+            <a href="${BASE_URL}/confirm-email?code=${user.emailConfirmation.confirmationCode}">${BASE_URL}/confirm-email?code=${user.emailConfirmation.confirmationCode}</a>
         </div>`;
     },
 };

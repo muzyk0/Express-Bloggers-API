6e4ae69dbc429110b7d93fa46e4d8167ed90d1f9
muzyk0 <ru9art@gmail.com>
2022-07-06 23:09:15 +0300

    handle error in send email

--- src/domain/email-service.ts
+++ src/domain/email-service.ts
@@ -16,20 +16,24 @@ export class EmailService {
             },
         });
 
-        // send mail with defined transport object
-        let info = await transporter.sendMail({
-            from: '"9ART.ru 👻" <info@9art.ru>', // sender address
-            to: email, // list of receivers
-            subject, // Subject line
-            // text: "Hello world?", // plain text body
-            html: template, // html body
-        });
+        try {
+            // send mail with defined transport object
+            const info = await transporter.sendMail({
+                from: '"9ART.ru 👻" <info@9art.ru>', // sender address
+                to: email, // list of receivers
+                subject, // Subject line
+                // text: "Hello world?", // plain text body
+                html: template, // html body
+            });
 
-        console.log('Message sent: %s', info);
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            console.log('Message sent: %s', info);
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        } catch (e) {
+            console.log(`email isn't send. Error: ` + e);
+        }
     }
 }
